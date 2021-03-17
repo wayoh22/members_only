88c518bbc0dd889ea5a53edc0700f5cc28c798e4
Ronnie <ronnie.tamburro@gmail.com>
2021-03-17 07:00:27 -0400

    Added relationships between users and posts. Also setup HTML for logging in, password reset, and signing up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /posts or /posts.json
   def index
     @posts = Post.all.order("created_at DESC")
+    @post = Post.new
   end
 
   # GET /posts/1 or /posts/1.json
